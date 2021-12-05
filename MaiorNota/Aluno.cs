76e4a62b4037009613be41e0e62c2a75b907566a
DiegoLins10 <diegofernandeslins@gmail.com>
2021-12-04 22:37:53 -0300

    Add project files.

---
+++ MaiorNota/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaiorNota
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int Nota { get; set; }
+
+        public Aluno(string nome, int nota)
+        {
+            Nome = nome;
+            Nota = nota;
+        }
+    }
+}
---
+++ MaiorNota/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaiorNota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero de alunos");
+
+            int quant = Convert.ToInt32(Console.ReadLine());
+            Aluno[] alunos = new Aluno[quant];
+
+
+            Console.WriteLine("Nome do aluno(ENTER) Sua nota");
+
+            for (int valorX = 0; valorX < alunos.Length; valorX++)
+            {
+                Console.WriteLine("Aluno: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("nota: ");
+                int nota = int.Parse(Console.ReadLine());
+                Aluno aluno = new Aluno(nome, nota);
+                alunos[valorX] = aluno;
+            }
+
+            int maiorNota = 0;
+            string NomeMaiorNota = "";
+            foreach (var i in alunos)
+            {
+                if (i.Nota > maiorNota)
+                {
+                    maiorNota = i.Nota;
+                    NomeMaiorNota = i.Nome;
+                }
+            }
+
+            Console.WriteLine("A maior nota foi " + maiorNota);
+
+        }
+    }
+}
